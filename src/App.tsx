c2c8d35171bed944c914c9d0b2e1e3aac0ac7e9c
maksim <krovorgen@gmail.com>
2023-03-10 21:45:22 +0300

    обработка если сайт лёг

--- src/App.tsx
+++ src/App.tsx
@@ -8,53 +8,62 @@ import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import styles from './App.module.scss';
 
 export const App = () => {
   const [posts, setPosts] = useState<{ title: string; channel: string }[]>([]);
+  const [isDeathBackend, setIsDeathBackend] = useState(false);
+
   useEffect(() => {
     axios
       .get('https://backenddoublemax.space/', { withCredentials: true })
       .then((res) => {
         setPosts(res.data);
       })
-      .catch(console.log);
+      .catch(() => {
+        setIsDeathBackend(true);
+      });
   }, []);
 
   return (
     <>
-      <Swiper
-        className="blog-slider"
-        modules={[Pagination, EffectFade]}
-        spaceBetween={750}
-        effect={'fade'}
-        loop={true}
-        mousewheel={{ invert: false }}
-        pagination={{ clickable: true, el: '.blog-slider__pagination' }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}>
-        {posts &&
-          posts.map((el, index) => {
-            return (
-              <SwiperSlide key={index} className={'blog-slider__item'}>
-                <div className="blog-slider__img">
-                  <img
-                    src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp"
-                    alt=""
-                  />
-                </div>
-                <div className="blog-slider__content">
-                  <span className="blog-slider__code">26 December 2019</span>
-                  <div className="blog-slider__title">{el.title}</div>
-                  <div className="blog-slider__text">{el.channel}</div>
-                  <a href="/" className="blog-slider__button">
-                    READ MORE
-                  </a>
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        <div className="blog-slider__pagination"></div>
-      </Swiper>
+      {isDeathBackend ? (
+        <p className={styles.deathBackend}>БЭК УМЕР КАК ПОСЛЕДНЯЯ ТВАРЬ</p>
+      ) : (
+        <Swiper
+          className="blog-slider"
+          modules={[Pagination, EffectFade]}
+          spaceBetween={750}
+          effect={'fade'}
+          loop={true}
+          mousewheel={{ invert: false }}
+          pagination={{ clickable: true, el: '.blog-slider__pagination' }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}>
+          {posts &&
+            posts.map((el, index) => {
+              return (
+                <SwiperSlide key={index} className={'blog-slider__item'}>
+                  <div className="blog-slider__img">
+                    <img
+                      src="https://res.cloudinary.com/muhammederdem/image/upload/q_60/v1535759872/kuldar-kalvik-799168-unsplash.webp"
+                      alt=""
+                    />
+                  </div>
+                  <div className="blog-slider__content">
+                    <span className="blog-slider__code">26 December 2019</span>
+                    <div className="blog-slider__title">{el.title}</div>
+                    <div className="blog-slider__text">{el.channel}</div>
+                    <a href="/" className="blog-slider__button">
+                      READ MORE
+                    </a>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          <div className="blog-slider__pagination"></div>
+        </Swiper>
+      )}
     </>
   );
 };
